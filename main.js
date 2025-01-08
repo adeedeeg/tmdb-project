ee8ecf09928bc79c9401b002db8e9da100a7acdc
adeedeeg <aditogrg@gmail.com>
2025-01-08 23:48:03 +0000

    added log in forms

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-const form = document.querySelector('form');
+const searchForm = document.getElementById('searchForm');
 const apiKey = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDlkMWNmNzVmYjI2ZjljNzQ4Y2JlZjlhOTdiZWVhMyIsIm5iZiI6MTczNDY1NDY5My4zNTYsInN1YiI6IjY3NjRiYWU1MWIwNmM1ZjI4Yjc0ODYyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SgSWhGUA5UcgHWmTj0f5E5wZzgofoNsjmN6sD4MNDoM'
 const watchlistAdd = document.getElementById('watchlist-list');
+// const logInForm = document.getElementById('LogInForm');
 
+// filterButtonsWrapper();
 
-filterButtonsWrapper();
+// //log in button pressed
+// logInForm.addEventListener('submit',(l) =>{
+//   l.preventDefault();
+
+//   const displaySidebar = document.getElementByClassName('sidebar-watchlist');
+//   displaySidebar.innterHTML = 'My Watchlist';
+
+// })
+
 
 //use input to search api
-form.addEventListener('submit', (e) => {
+searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const displayTitleCard = document.getElementById('title-card-store');//CHECK WHY THIS IS REQUIRED
+  const displayTitleCard = document.getElementById('title-card-store');
   displayTitleCard.innerHTML = '';
 
-  let query = form.querySelector('input').value;
+  let query = searchForm.querySelector('input').value;
   console.log(query);
 
   tmdbApi(query);
